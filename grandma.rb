dd08f6ee237989901e6f5cdcec648aa101cb51c1
John Hayes <johnnyhayes7@gmail.com>
2019-01-16 13:57:56 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(greeting)
   if greeting == greeting.upcase
     return "NO, NOT SINCE 1938!"
-  elsif greeting = "I LOVE YOU GRANDMA!"
+  elsif greeting == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   else
     return "HUH?! SPEAK UP SONNY!"
